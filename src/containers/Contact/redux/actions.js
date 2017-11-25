250972a0626a34efc1651235769173f3d2b673f8
grady-lad <ogradymartin7@gmail.com>
2017-11-25 14:35:58 -0500

    Redux boilerplate and updating mail endpoint

--- src/containers/Contact/redux/actions.js
+++ src/containers/Contact/redux/actions.js
@@ -2,6 +2,9 @@ import {
   CONTACT_ITEMS_LOAD,
   CONTACT_ITEMS_SUCCESS,
   CONTACT_ITEMS_FAILURE,
+  CONTACT_SEND_MAIL,
+  CONTACT_SEND_MAIL_SUCCESS,
+  CONTACT_SEND_MAIL_FAILURE,
 } from './constants';
 
 export const getContactItems = () => ({
@@ -20,5 +23,28 @@ export const contactItemsSuccess = ({ payload: data }) => ({
 
 export const contactItemsFailure = error => ({
   type: CONTACT_ITEMS_FAILURE,
+  error,
+});
+
+export const sendMail = () => ({
+  types: [
+    CONTACT_SEND_MAIL,
+    CONTACT_SEND_MAIL_SUCCESS,
+    CONTACT_SEND_MAIL_FAILURE,
+  ],
+  payload: {
+    request: {
+      url: '/mail-proxy/v1/email',
+    },
+  },
+});
+
+export const sendMailSuccess = ({ payload: data }) => ({
+  type: CONTACT_SEND_MAIL_SUCCESS,
+  payload: { data },
+});
+
+export const sendMailFailure = error => ({
+  type: CONTACT_SEND_MAIL_FAILURE,
   error,
 });
--- src/containers/Contact/redux/reducer.js
+++ src/containers/Contact/redux/reducer.js
@@ -2,26 +2,58 @@ import {
   CONTACT_ITEMS_LOAD,
   CONTACT_ITEMS_SUCCESS,
   CONTACT_ITEMS_FAILURE,
+  CONTACT_SEND_MAIL,
+  CONTACT_SEND_MAIL_SUCCESS,
+  CONTACT_SEND_MAIL_FAILURE,
 } from './constants';
 
 const initialState = {
   loading: true,
   contactItems: {},
+  loadingMail: false,
+  mailResponse: false,
+  mailError: '',
 };
 
 function contactReducer(state = initialState, action) {
   switch (action.type) {
-    case CONTACT_ITEMS_LOAD:
+    case CONTACT_ITEMS_LOAD: {
       return { ...state, loading: true };
-
+    }
     case CONTACT_ITEMS_SUCCESS: {
       const { data } = action.payload;
       const { acf } = data[0];
       return { ...state, loading: false, contactItems: acf || [] };
     }
 
-    case CONTACT_ITEMS_FAILURE:
+    case CONTACT_ITEMS_FAILURE: {
       return { ...state, loading: false, error: '' };
+    }
+    case CONTACT_SEND_MAIL:
+      return {
+        ...state,
+        loadingMail: true,
+        mailResponse: false,
+        mailError: false,
+      };
+
+    case CONTACT_SEND_MAIL_SUCCESS: {
+      return {
+        ...state,
+        loadingMail: false,
+        mailResponse: true,
+        mailError: false,
+      };
+    }
+    case CONTACT_SEND_MAIL_FAILURE: {
+      const { error } = action.paylod;
+      return {
+        ...state,
+        loadingMail: false,
+        mailResponse: false,
+        error,
+      };
+    }
 
     default:
       return state;
