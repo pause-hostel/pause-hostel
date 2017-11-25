8fba468d09c42b9af5c9add54d016d639c0a6e0f
Stephanie Tassone <stephanie.tassone@gmail.com>
2017-11-25 13:59:46 -0500

    Refactor rooms and create contact form

--- src/components/RoomImages/index.js
+++ src/components/RoomImages/index.js
@@ -4,9 +4,13 @@ import './roomImages.css';
 
 const RoomImages = ({ rooms }) => (
   <div className="room-container">
-    {Object.values(rooms).map((room, idx) => (
+    {rooms.map(({ room }, idx) => (
       <div className="room-wrapper" key={`room-${idx}`}>
-        <img className="room-image" src={room.room_image} alt="" />
+        <div
+          className="room-image"
+          style={{ backgroundImage: `url(${room.room_image})` }}
+          alt=""
+        />
         {room.book_button ? (
           <a className="book-button-room" href={room.book_button.url}>
             {room.book_button.title}
@@ -22,10 +26,10 @@ const RoomImages = ({ rooms }) => (
 );
 
 RoomImages.propTypes = {
-  rooms: PropTypes.object, //eslint-disable-line
+  rooms: PropTypes.array, //eslint-disable-line
 };
 RoomImages.defaultProps = {
-  rooms: {},
+  rooms: [],
 };
 
 export default RoomImages;
