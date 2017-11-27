a018f7b96d5f71475d965840d51998eadc98ed85
grady-lad <ogradymartin7@gmail.com>
2017-11-26 22:12:25 -0500

    Adding page title for the site

--- pause-theme/index.php
+++ pause-theme/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <title><?php bloginfo('name') ?></title>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
